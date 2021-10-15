dd8683f402732bcc7b34a99402c457176cb1fa53
Daniel Fergisz <daniel.fergisz@gmail.com>
2021-10-15 11:33:05 +0200

    Modyfikacja kodu - dodanie informacji do konsoli

--- src/test/java/Nawigowanie.java
+++ src/test/java/Nawigowanie.java
@@ -8,8 +8,9 @@ public class Nawigowanie {
 
     @Test
     public void navigation() {
-        System.out.println("Uruchamianie Testu..");
+        System.out.println("================ Uruchamianie testu ================");
         System.setProperty("webdriver.chrome.driver", "G:/WebDrivers2/chromedriver.exe");
+        System.out.println("Uruchamianie WebDrivera..");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
@@ -17,21 +18,28 @@ public class Nawigowanie {
         driver.manage().window().maximize();
 
         //Przejdź na stronę repairbox.pl
-        driver.navigate().to("https://www.repairbox.pl/");
+        String www = "https://www.repairbox.pl/";
+        System.out.println("Przejdź do strony: " + www);
+        driver.navigate().to(www);
 
         //Przejdź do zakładki Kontakt
+        System.out.println("Przejdź do zakładki: Kontakt");
         driver.navigate().to("https://www.repairbox.pl/kontakt/");
 
         //Wykonaj polecenie "wstecz" na przeglądarce
+        System.out.println("Powrót do poprzedniej zakładki..");
         driver.navigate().back();
 
         //Wykonaj polecenie "dalej" na przeglądarce
+        System.out.println("Przejdź Dalej..");
         driver.navigate().forward();
 
         //Odśwież stronę
+        System.out.println("Odśwież stronę..");
         driver.navigate().refresh();
 
         //Zamknij przeglądarkę
+        System.out.println("================ Zakończenie testu ================");
         driver.quit();
     }
 }
